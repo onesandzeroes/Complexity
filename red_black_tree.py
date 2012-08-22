71ebdb6fa60fe86923fadfa1a75ee61ced43a38a
onesandzeroes <marius.mather@gmail.com>
2012-08-22 22:21:53 +1000

    Moved add() into RBTree Makes more sense to have the add function in the tree class, to allow easier checks of the tree's properties

--- red_black_tree.py
+++ red_black_tree.py
@@ -5,11 +5,26 @@ import blessings
 class RBTree:
     def __init__(self, vals, with_term=False):
         first = vals.pop(0)
-        self.root = RBNode(first, 'B')
+        self.root = RBNode(NullNode(), first, 'B')
         for val in vals:
-            self.root.add(val)
+            self.add(self.root, val)
         if with_term:
             self.t = blessings.Terminal()
+
+    def add(self, target_node, new_val):
+        if new_val == target_node.value:
+            print(new_val, " already exists")
+            return
+        if new_val < target_node.value:
+            if not target_node.left:
+                target_node.left = RBNode(target_node, new_val, 'R')
+            else:
+                self.add(target_node.left, new_val)
+        elif new_val > target_node.value:
+            if not target_node.right:
+                target_node.right = RBNode(target_node, new_val, 'R')
+            else:
+                self.add(target_node.right, new_val)
 
     def term_print(self):
         print_col = {'R': self.t.red_on_white, 'B': self.t.black_on_white}
@@ -20,7 +35,8 @@ class RBTree:
 class RBNode:
     colours = ('R', 'B')
 
-    def __init__(self, value, colour):
+    def __init__(self, parent, value, colour):
+        self.parent = parent
         self.value = value
         self.left = NullNode()
         self.right = NullNode()
@@ -29,35 +45,17 @@ class RBNode:
     def __bool__(self):
         return True
 
-    def add(self, new_val):
-        if self.colour == 'R':
-            child_colour = 'B'
-        elif self.colour == 'B':
-            child_colour = 'R'
-        if new_val < self.value:
-            if not self.left:
-                self.left = RBNode(new_val, child_colour)
-            else:
-                self.left.add(new_val)
-        elif new_val > self.value:
-            if not self.right:
-                self.right = RBNode(new_val, child_colour)
-            else:
-                self.right.add(new_val)
-
     def __str__(self):
         return str(self.value)
 
 
-class NullNode:
+class NullNode(RBNode):
     def __init__(self):
         self.colour = 'B'
+        self.value = 'Null'
 
     def __bool__(self):
         return False
-
-    def __str__(self):
-        return 'Null'
 
 if __name__ == '__main__':
     tree1 = RBTree([13, 17, 8, 11, 15, 1, 25, 6, 22, 27])
