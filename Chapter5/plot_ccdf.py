94436d5b763b4c47c3879181672e6328eb082b0d
oneandzeroes <onesandzeroes@seabeast.com>
2012-10-23 00:04:13 +1100

    Complimentary cdf for Pareto distribution

--- Chapter5/plot_ccdf.py
+++ Chapter5/plot_ccdf.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as pyplot
 import Cdf
 
 
-def plot_ccdf(values, probs, title='', lambd=0):
+def plot_ccdf_exp(values, probs, title='', lambd=0):
     """
     Plot the complimentary CDF (1 - CDF) of a given set of values and
     probabilities, to check if it's exponentially distributed, i.e.
@@ -22,13 +22,37 @@ def plot_ccdf(values, probs, title='', lambd=0):
     pyplot.show()
 
 
-def test_ccdf(size):
+def test_ccdf_exp(size):
     expo_sample = [random.expovariate(0.2) for n in range(size)]
     expo_cdf = Cdf.MakeCdfFromList(expo_sample)
-    plot_ccdf(*expo_cdf.Render(), title="Exponential Sample", lambd=0.2)
+    plot_ccdf_exp(*expo_cdf.Render(), title="Exponential Sample", lambd=0.2)
     normal_sample = [random.gauss(5, 1) for n in range(size)]
     normal_cdf = Cdf.MakeCdfFromList(normal_sample)
-    plot_ccdf(*normal_cdf.Render(), title="Normal Sample", lambd=0.2)
+    plot_ccdf_exp(*normal_cdf.Render(), title="Normal Sample", lambd=0.2)
+
+
+def plot_ccdf_pareto(values, probs, alpha=0):
+    """
+    Plot log(x) against log(ccdf(x)) for a quick check of whether a sample
+    is Pareto-distributed- should give a straight line with a slope of
+    -alpha (where alpha determines the shape of the distribution) and
+    an intercept of alpha * log(x_m) (where x_m determines the location
+    of the distribution)
+    """
+    log_xs = [math.log(x) for x in values]
+    ys = [(1 - p) for p in probs]
+    log_ys = [math.log(y) if y > 0 else None for y in ys]
+    fitted_line = [(-1 * alpha * x) for x in log_xs]
+    pyplot.plot(log_xs, log_ys, 'ro', log_xs, fitted_line, 'b-')
+    pyplot.xlabel("Log(x)")
+    pyplot.ylabel("Log(y)")
+    pyplot.show()
+
+
+def test_ccdf_pareto(size, alpha):
+    pareto_sample = [random.paretovariate(alpha) for n in range(size)]
+    pareto_cdf = Cdf.MakeCdfFromList(pareto_sample)
+    plot_ccdf_pareto(*pareto_cdf.Render(), alpha=alpha)
 
 if __name__ == '__main__':
-    test_ccdf(10000)
+    test_ccdf_pareto(10000, 2)
